fcb5dd827537444cf3acc9092c0b4980fda28fd0
James Czekaj <james@James-PC>
2024-02-09 17:11:46 +0000

    Draw garden grid and implement simple seed packet dragging

--- game.c
+++ game.c
@@ -1,4 +1,6 @@
 #include <raylib.h>
+#include <raymath.h>
+#include <stdbool.h>
 
 int main(void)
 {
@@ -11,17 +13,45 @@ int main(void)
 
     Vector2 drawPos = { 100, 100};
 
+    int gardenGrid[9][5];
+    Vector2 gridDrawOffset = {40, 80};
+    int gridCellGap = 4;
+    Vector2 gridCellSize = {55, 70};
+
+    Vector2 seedPacketOrigin = {50, 10};
+    Vector2 seedPacketSize = {50, 50};
+    bool draggingSeedPacket = false;
+
     while (!WindowShouldClose()) {
 
-        if (IsMouseButtonPressed(MOUSE_BUTTON_LEFT)) {
-            drawPos = GetMousePosition();
+        if (IsMouseButtonPressed(MOUSE_BUTTON_LEFT) && !draggingSeedPacket) {
+            Vector2 mPos = GetMousePosition();
+
+            if (CheckCollisionPointRec(mPos, (Rectangle){seedPacketOrigin.x, seedPacketOrigin.y, seedPacketSize.x, seedPacketSize.y})) {
+                draggingSeedPacket = true;
+            }
+        }
+
+        if (IsMouseButtonReleased(MOUSE_BUTTON_LEFT)) {
+            draggingSeedPacket = false;
         }
 
         BeginDrawing();
 
         ClearBackground(WHITE);
 
-        DrawCircleV(drawPos, 32, (Color){204, 100, 25, 255});
+        for (int x = 0; x < 9; x++) {
+            for (int y = 0; y < 5; y++) {
+                DrawRectangleV(Vector2Add(gridDrawOffset, (Vector2){x*gridCellSize.x+x*gridCellGap, y*gridCellSize.y+y*gridCellGap}), gridCellSize, RED);
+
+            }
+        }
+
+        if (draggingSeedPacket) {
+            DrawRectangleV(Vector2Subtract(GetMousePosition(), (Vector2){seedPacketSize.x/2, seedPacketSize.y/2}), seedPacketSize, BLUE);
+        } else {
+            DrawRectangleV(seedPacketOrigin, seedPacketSize, BLUE);
+        }
 
         EndDrawing();
 
