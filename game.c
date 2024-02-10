366f5d1e10830b9aabfa1287623fddf7e22a14fe
James Czekaj <james@James-PC>
2024-02-10 20:10:14 +0000

    Implement projectiles shooting from peashooter

--- game.c
+++ game.c
@@ -27,6 +27,23 @@ typedef struct {
     float health;
 } Plant;
 
+typedef struct {
+    Vector2 pos;
+    bool active;
+} Projectile;
+
+
+void UpdateDrawPShooter(Plant* p, Vector2 screenPos);
+
+// Assets
+Texture2D seedPacketSprite;
+Texture2D lawnBackgroundSprite;
+Texture2D pShooterSprite;
+Texture2D peaSprite;
+
+#define MAX_PROJ 16
+Projectile projectiles[MAX_PROJ] = {0};
+int nextProjectile = 0;
 
 int main(void)
 {
@@ -37,10 +54,11 @@ int main(void)
 
     SetTargetFPS(60);
 
-    Texture2D seedPacketSprite = LoadTexture("sprites/seedpacket.png");
-    Texture2D lawnBackgroundSprite = LoadTexture("sprites/lawn.png");
+    seedPacketSprite = LoadTexture("sprites/seedpacket.png");
+    lawnBackgroundSprite = LoadTexture("sprites/lawn.png");
+    pShooterSprite = LoadTexture("sprites/pshooter.png");
+    peaSprite = LoadTexture("sprites/pea.png");
 
-    Vector2 drawPos = { 100, 100};
 
     Plant gardenGrid[9][5] = {0};
     Vector2 gridDrawOffset = {40, 80};
@@ -72,6 +90,9 @@ int main(void)
             seedPacket.dragging = false;
             ShowCursor();
         }
+
+        // Update plants
+
 
         BeginDrawing();
 
@@ -111,13 +132,27 @@ int main(void)
             }
         }
 
-        // Draw plants
+        // Update and Draw plants
         for (int x = 0; x < 9; x++) {
             for (int y = 0; y < 5; y++) {
-                if (gardenGrid[x][y].type != PT_NONE) {
-                    Vector2 gridCellPos = Vector2Add(gridDrawOffset, (Vector2){x*gridCellSize.x+x*gridCellGap, y*gridCellSize.y+y*gridCellGap});
-                    DrawRectangleV(gridCellPos, gridCellSize, BLUE);
+                Vector2 screenPos = Vector2Add(gridDrawOffset, (Vector2){x*gridCellSize.x+x*gridCellGap, y*gridCellSize.y+y*gridCellGap});
+                Plant* p = &gardenGrid[x][y];
+                switch (p->type) {
+                    case PT_PSHOOTER:
+                        UpdateDrawPShooter(p, screenPos);
+                        break;
+                    case PT_NONE:
+                    default:
+                        continue;
                 }
+            }
+        }
+
+        // Update and draw Projectiles
+        for (int i = 0; i < 16; i++) {
+            if (projectiles[i].active) {
+                projectiles[i].pos.x += 5;
+                DrawTextureV(peaSprite, projectiles[i].pos, WHITE);
             }
         }
 
@@ -136,4 +171,20 @@ int main(void)
     CloseWindow();
 
     return 0;
+}
+
+void UpdateDrawPShooter(Plant* p, Vector2 screenPos)
+{
+    p->cooldown--;
+    if (p->cooldown <= 0) {
+        p->cooldown = 120;
+        if (nextProjectile == MAX_PROJ) {
+            nextProjectile = 0;
+        }
+        projectiles[nextProjectile].active = true;
+        projectiles[nextProjectile].pos = Vector2Add(screenPos, (Vector2){16, 30});
+        nextProjectile++;
+    }
+    DrawTexture(pShooterSprite, screenPos.x, screenPos.y, WHITE);
+
 }
